6b5164586e26b8182c1775cc957365cf34f90706
FahdAzouz <fahdstudies00@gmail.com>
2024-06-25 18:59:01 +0100

    task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -10,18 +10,30 @@ class BasicCache(BaseCaching):
         put(key, item) - store a key-value pair
         get(key) - retrieve the value associated with a key
     """
+
     def __init__(self):
-        '''initialize'''
+        """
+        Initialize the class using the parent class __init__ method
+        """
         BaseCaching.__init__(self)
 
     def put(self, key, item):
-        '''put data in the cache'''
+        """
+        Store a key-value pair
+        Args:
+            Key
+            Item
+        """
         if key is None or item is None:
             pass
-        self.cache_data[key] = item
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
-        '''get item from cache'''
-        if key is None or key not in self.cache_data.keys():
-            return None
-        return self.cache_data[key]
+        """
+        Return value linked to key.
+        If key is None or doesn't exist, return None
+        """
+        if key is not None and key in self.cache_data.keys():
+            return self.cache_data[key]
+        return None
