3789968bb39f262a9747b58216b61a4b0fddb2e3
FahdAzouz <fahdstudies00@gmail.com>
2024-06-25 18:51:37 +0100

    task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+''''''
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    Defines a class for caching information in key-value pairs
+    Methods:
+        put(key, item) - store a key-value pair
+        get(key) - retrieve the value associated with a key
+    """
+    def __init__(self):
+        '''initialize'''
+        BaseCaching.__init__(self)
+
+    def put(self, key, item):
+        '''put data in the cache'''
+        if key is None or item is None:
+            pass
+        self.cache_data[key] = item
+
+    def get(self, key):
+        '''get item from cache'''
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
