b98bac192bf5975a4da80f726650f5b433c6cd50
FahdAzouz <fahdstudies00@gmail.com>
2024-07-02 23:56:35 +0100

    clean

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python3
-"""simple app setup"""
-
+"""
+Flask app
+"""
 from flask import Flask, render_template
 from flask_babel import Babel
 
 
-class Config():
-    """Configuration of babel"""
+class Config(object):
+    """
+    Configuration for Babel
+    """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCAL = "en"
+    BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-babel = Bable(app)
+babel = Babel(app)
 
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
-    """render the html file"""
-    return render_template("1-index.html")
+    """
+    Handles / route
+    """
+    return render_template('1-index.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-"""simple app setup"""
-
+"""
+Flask app
+"""
 from flask import (
     Flask,
     render_template,
@@ -9,29 +10,35 @@ from flask import (
 from flask_babel import Babel
 
 
-class Config():
-    """Configuration of babel"""
+class Config(object):
+    """
+    Configuration for Babel
+    """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCAL = "en"
+    BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-babel = Bable(app)
+babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
-    '''get the local language'''
-    return request.accept_languages.best_match(ap.config['LANGUAGES'])
+    """
+    Select and return best language match based on supported languages
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
-    """render the html file"""
-    return render_template("2-index.html")
+    """
+    Handles / route
+    """
+    return render_template('2-index.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
