e095f4dbba64db8560bf5f05771af560a09edd80
FahdAzouz <fahdstudies00@gmail.com>
2024-07-02 19:00:44 +0100

    task 0

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,16 +2,25 @@
 """simple app setup"""
 
 from flask import Flask, render_template
+from flask_babel import Babel
 
 
 app = Flask(__name__)
+babel = Bable(app)
 
+class Config():
+    """Configuration of babel"""
+    LANGUAGES = ["en", "fr"]
+
+@babel.localeselector
+def get_lang():
+    conf = new Config()
+    return conf.LANGUAGES["en"]
 
 @app.route('/')
 def index():
     """render the html file"""
     return render_template("templates/index.html")
-
 
 if __name__ == '__main__':
     app.run(debug=True)
